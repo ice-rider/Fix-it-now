cd87be9a1046ff8f342b0327c7fe1b6a93ac0b44
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-10-23 15:42:49 +0400

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flasgger import Swagger
+
+from models import db
 from resources import api
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('PRODUCTION_DATABASE_URL')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+
+db.init_app(app)
 
 # initialization app to api
 api.init_app(app)
