3de04a58cae62d267f4c28ad46e53f7c4fa216b0
for2codespace <148958459+for2codespace@users.noreply.github.com>
2023-10-31 20:08:40 +0000

    Добавлена страничка / (<MainPage />)

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,36 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserView } from 'react-device-detect';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// Desktop version:
+import MainPage from './desktop/Pages/MainPage';
 
 function App() {
   return (
-    <h1>Fix it now</h1>  
+    <>
+      <BrowserView>
+      <BrowserRouter>
+        {/* desktop version */}
+        <Routes>
+          <Route path='*' element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+      </BrowserView>
+      
+      {/* toastify container for notifications */}
+      <ToastContainer
+				position="bottom-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="colored" />
+    </>
   );
 }
 
