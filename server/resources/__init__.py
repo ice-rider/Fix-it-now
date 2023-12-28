d496b1a27772ab78a4d74eb1af63838f7e46c5dd
codevoke <119005542+codevoke@users.noreply.github.com>
2023-12-28 07:16:25 +0400

    Update __init__.py

--- server/resources/__init__.py
+++ server/resources/__init__.py
@@ -1,10 +1,10 @@
 from flask_restful import Api
 from .auth import Auth, Logout
-from .ticket import Ticket
+from .ticket import Ticket, TicketList
 from .user import User
 
 
 api = Api(prefix="/api")
 
-for resource in [Auth, Logout, Ticket, User]:
+for resource in [Auth, Logout, Ticket, TicketList, User]:
     api.add_resource(resource, resource.path)
