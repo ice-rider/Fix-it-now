4f3a16818a6a6e5155e220438a931f006ca4be14
exp-of-betrayal <glebisi@yandex.ru>
2024-01-14 14:59:11 +0400

    update index

--- client/src/desktop/components/Ticket.js
+++ client/src/desktop/components/Ticket.js
@@ -13,18 +13,6 @@ export default function Ticket({ ticket }) {
 
     return (
         <div className="TicketCard">
-            <div className='TicketAuthorBlock'>
-                <div className="TicketAuthorAvatar"></div>
-                <div className="TicketAuthorName">{author.username}</div>
-                <div className="TicketDate">{ticket.}</div>
-            </div>
-            <div className="TicketPhotoBlock">
-                <img className="TicketPhotoImage" src={ticket.photo} />
-            </div>
-            <div className="TicketInfo" data-id={ticket.id}>
-                <div className="TicketSection">{ticket.section}</div>
-                <div className="TicketLocation">{ticket.location}</div>
-            </div>
         </div>
     );
 }
