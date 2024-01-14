34c49fc1bd617f52f14728216f64ccc728e11327
exp-of-betrayal <glebisi@yandex.ru>
2024-01-14 19:24:17 +0400

    Update Ticket.js

--- client/src/desktop/components/Ticket.js
+++ client/src/desktop/components/Ticket.js
@@ -12,7 +12,15 @@ export default function Ticket({ ticket }) {
     }, [])
 
     return (
-        <div className="TicketCard">
+        <div className="TicketView">
+            <div className="TicketPhotoWrapper">
+                <img className="TicketPhoto" src={ticket.photo_url} />
+            </div>
+            <div className="TicketInfo">
+                <div className="TicketSection"> {ticket.title} </div>
+                <div className="TicketDescription"> {ticket.description} </div>
+                <div className="TicketAuthor"> Автор: {author.name} </div>
+            </div>
         </div>
     );
 }
